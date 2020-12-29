8af2e942a0049d0035337995a6a199e1509ec76c
carlos molero <cmolero130@gmail.com>
2020-12-29 18:22:09 -0500

    created form missing styling

--- react-app/src/components/WelcomePage.js
+++ react-app/src/components/WelcomePage.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function WelcomePage() {
     return (
         <div className="welcomePage">
-            hello
+            <div className="welcomePage_form">
+                <form>
+                    <h1 className="welcomePage_form-title">Register Form</h1>
+                    <label className="welcomePage_form-input">
+                        Email
+                        <input />
+                    </label>
+                    <br />
+                    <label className="welcomePage_form-input">
+                        Name
+                        <input />
+                    </label>
+                    <br/>
+                    <label className="welcomePage_form-input">
+                        Password
+                        <input />
+                    </label>
+                    <br />
+                    <label className="welcomePage_form-input">
+                        Confirmed Password
+                        <input />
+                    </label>
+                    <button className="welcomePage_form-submit" type="submit">
+                        SignUp
+                    </button>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
