bfa765d2ecb6f5f7a62096b8686897f92e4bbff4
carlos molero <cmolero130@gmail.com>
2020-12-29 18:46:27 -0500

    added switch and redirect to app.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
@@ -11,54 +11,75 @@ import Routes from "./components/Routes";
 import WelcomePage from "./components/WelcomePage";
 
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loaded, setLoaded] = useState(false);
-  const [user, setUser] = useState(null);
+    const [authenticated, setAuthenticated] = useState(false);
+    const [loaded, setLoaded] = useState(false);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    (async() => {
-      const userData = await authenticate();
-      if (!userData.errors) {
-        setAuthenticated(true);
-        setUser(userData);
-      }
-      setLoaded(true);
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const userData = await authenticate();
+            if (!userData.errors) {
+                setAuthenticated(true);
+                setUser(userData);
+            }
+            setLoaded(true);
+        })();
+    }, []);
 
-  if (!loaded) {
-    return null;
-  }
+    if (!loaded) {
+        return null;
+    }
 
-  return (
-    <BrowserRouter>
-      <NavBar setAuthenticated={setAuthenticated} />
-      <Route>
-        <WelcomePage/>
-      </Route>
-      <Route path="/login" exact={true}>
-        <LoginForm
-          authenticated={authenticated}
-          setAuthenticated={setAuthenticated}
-        />
-      </Route>
-      <Route path="/sign-up" exact={true}>
-        <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
-      </Route>
-      <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-        <UsersList/>
-      </ProtectedRoute>
-      <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
-        <User />
-      </ProtectedRoute>
-      <ProtectedRoute path="/routes" exact={true} authenticated={authenticated}>
-        <Routes user={user}/>
-      </ProtectedRoute>
-      <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-        <h1>My Home Page</h1>
-      </ProtectedRoute>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <NavBar setAuthenticated={setAuthenticated} />
+            <Switch>
+                <Route exact path="/welcome">
+                    <WelcomePage />
+                </Route>
+                <Route path="/login" exact={true}>
+                    <LoginForm
+                        authenticated={authenticated}
+                        setAuthenticated={setAuthenticated}
+                    />
+                </Route>
+                <Route path="/sign-up" exact={true}>
+                    <SignUpForm
+                        authenticated={authenticated}
+                        setAuthenticated={setAuthenticated}
+                    />
+                </Route>
+                <ProtectedRoute
+                    path="/users"
+                    exact={true}
+                    authenticated={authenticated}
+                >
+                    <UsersList />
+                </ProtectedRoute>
+                <ProtectedRoute
+                    path="/users/:userId"
+                    exact={true}
+                    authenticated={authenticated}
+                >
+                    <User />
+                </ProtectedRoute>
+                <ProtectedRoute
+                    path="/routes"
+                    exact={true}
+                    authenticated={authenticated}
+                >
+                    <Routes user={user} />
+                </ProtectedRoute>
+                <ProtectedRoute
+                    path="/"
+                    exact={true}
+                    authenticated={authenticated}
+                >
+                    <h1>My Home Page</h1>
+                </ProtectedRoute>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-const ProtectedRoute = props => {
-  return (
-    <Route {...props}>
-      {(props.authenticated)? props.children  : <Redirect to="/welcome" />}
-    </Route>
-  )
-
+const ProtectedRoute = (props) => {
+    return (
+        <Route {...props}>
+            {props.authenticated ? props.children : <Redirect to="/welcome" />}
+        </Route>
+    );
 };
 
 export default ProtectedRoute;
