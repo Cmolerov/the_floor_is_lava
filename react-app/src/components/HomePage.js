afa549f12d69bbbd8eb2d76026ad48d00d7a0278
carlos molero <cmolero130@gmail.com>
2020-12-30 14:09:58 -0500

    style homepage

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function HomePage() {
     return (
-        <div>
-         <h1>Home Page</h1>   
+        <div className="homePage_container">
+            <div className="homePage_container-left">
+                <div className="homePage_container-left_top"></div>
+                <div className="homePage_container-left_bottom"></div>
+            </div>
+            <div className="homePage_container-middle">
+                <div className="homePage_container-middle-container">
+                    <h1>Activity of the Week</h1>
+                </div>
+            </div>
+            <div className="homePage_container-right">
+                <div className="homePage_container-left_top">
+                    <h1>Explore New Routes</h1>
+                </div>
+                <div className="homePage_container-left_bottom">
+                    <h1>Route of the Day</h1>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
