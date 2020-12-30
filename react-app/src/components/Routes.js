30b352ffe90ebd0725606f3cfb95e4b1ecf9cd03
dale.sakamoto <daletsakamoto@gmail.com>
2020-12-29 16:50:02 -0800

    rendered a map

--- react-app/src/components/Routes.js
+++ react-app/src/components/Routes.js
@@ -1,14 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 import * as routesAction from '../store/routes'
+
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 function Routes(props) {
   const id = props.user.id;
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   const routes = useSelector(state => state.routes.routes);
+  const apiKey = useSelector(state => state.routes.apiKey)
+  console.log("THIS IS API KEY!!!!", apiKey)
   
   useEffect(() => {
     dispatch(routesAction.routesSearch(id))
@@ -16,15 +30,19 @@ function Routes(props) {
       .then(() => setIsLoaded(true))
   }, [dispatch])
   let userRoutes;
-  
+
   if (routes) {
     userRoutes =
       Object.values(routes).map((route, idx) => {
       return (
         <div key={idx} className='results-container__body__local'>
             <NavLink className='navlinks' to={`/routes/${route.id}`}>
-              <div className='results-local-header'>
-                {/* <img className='results-local-header__image' src={person.avatar /> */}
+            <div className='results-local-header'>
+              <div className='testCSS'>
+              {/* <img className='results-local-header__image' src={`https://maps.googleapis.com/maps/api/directions/json?
+origin=${route.startLat},${route.startLong}&destination=${route.endLat},${route.endLong}
+&key=${apiKey}`} /> */}
+              </div>
               <h1 className='results-local-username'>{route.name}</h1>
               </div>
             </NavLink>
@@ -37,9 +55,23 @@ function Routes(props) {
   }
 
   return isLoaded && (
+    <>
     <div>
       {userRoutes}
     </div>
+      <LoadScript
+        googleMapsApiKey={apiKey}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        >
+          { /* Child components, such as markers, info windows, etc. */ }
+          <></>
+        </GoogleMap>
+      </LoadScript>
+      </>
   )
 }
 
