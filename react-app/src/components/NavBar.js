718d69f2e5bf5314b85c03d5f63de7806a0e827f
carlos molero <cmolero130@gmail.com>
2020-12-31 13:32:50 -0500

    navBar text decoration

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -7,27 +7,27 @@ const NavBar = ({ setAuthenticated }) => {
     <nav className="navbar">
       <div className="navbar__grid-container">
         <div className="navbar__home">
-          <NavLink to="/" exact={true} activeClassName="active">
+          <NavLink className="navbar__grid-link" to="/" exact={true} activeClassName="active">
           <img className="img__icon" src='./images/favicon.ico' alt="volcano" size="50px"/> 
           </NavLink>
         </div>
           <div className="navbar__grid">
-            <NavLink to="/login" exact={true} activeClassName="active">
+            <NavLink className="navbar__grid-link" to="/login" exact={true} activeClassName="active">
               Login
             </NavLink>
           </div>
           <div className="navbar__grid">
-            <NavLink to="/sign-up" exact={true} activeClassName="active">
+            <NavLink className="navbar__grid-link" to="/sign-up" exact={true} activeClassName="active">
               Sign Up
             </NavLink>
         </div>
         <div className="navbar__grid">
-            <NavLink to="/routes" exact={true} activeClassName="active">
+            <NavLink className="navbar__grid-link" to="/routes" exact={true} activeClassName="active">
               Routes
             </NavLink>
           </div>
           <div className="navbar__grid">
-            <NavLink to="/users" exact={true} activeClassName="active">
+            <NavLink className="navbar__grid-link" to="/users" exact={true} activeClassName="active">
               Users
             </NavLink>
           </div>
