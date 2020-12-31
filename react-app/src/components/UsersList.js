607c6843871266cc5c0ca8a0076079159798603b
dale.sakamoto <daletsakamoto@gmail.com>
2020-12-31 10:35:19 -0800

    test out the map on the usersList tab

--- react-app/src/components/UsersList.js
+++ react-app/src/components/UsersList.js
@@ -1,51 +1,140 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { NavLink } from "react-router-dom";
+import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer, Marker} from '@react-google-maps/api';
 
 import * as routesAction from '../store/routes'
 
 function UsersList() {
   const dispatch = useDispatch()
   const [users, setUsers] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false)
   const routes = useSelector(state => state.routes.routes)
-  console.log("THESE ARE THE ROUTES", routes)
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("/api/users/");
-      const responseData = await response.json();
-      setUsers(responseData.users);
-    }
-    fetchData();
-  }, []);
+  const apiKey = useSelector(state => state.routes.apiKey)
+  const [currentLocation, setCurrentLocation] = useState(null)
+  const [id, setId] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [label, setLabel] = useState(false)
+  const [ markers, setMarkers ] = useState([]);
+  const [createMark, setcreateMark] = useState(false);
+  const [ myMap, setMyMap ] = useState(null);
+  const [center, setCenter] = useState(null);
+  let start = "start"
+  let end = "end"
 
   useEffect(() => {
     dispatch(routesAction.routesSearch(1))
-      .then(() => console.log("THE STORE IS UP AND RUNNING"))
-      .then(() => setIsLoaded(true))
+    .then(() => setIsLoaded(true))
   }, [dispatch])
 
   useEffect(() => {
     dispatch(routesAction.routeSearch(8))
   }, [dispatch])
 
-  const userComponents = users.map((user) => {
-    return (
-        <li key={user.id}>
-          <NavLink to={`/users/${user.id}`}>{user.username}</NavLink>
-        </li>
-    );
-  });
+  useEffect(() => {
+    console.log(markers)
+  }, [markers])
 
-  return isLoaded &&(
-    <>
-      <h1>User List: </h1>
-      <ul>{userComponents}</ul>
-      <div>Here's a route: {routes[1].name}</div>
-      <div>with a description: {routes[1].description}</div>
-    </>
-  );
+  useEffect(() => {
+    console.log(currentLocation)
+  }, [currentLocation])
+
+  useEffect(() => {
+    getLocation()
+}, [])
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          setCurrentLocation(pos)
+          setCenter(pos)
+        }, (err) => {
+          showError(err)
+        })
+    } else {
+      alert("Try another browser for geolocation services")
+    }
+  }
+  
+  function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        alert("You must allow location services to use this app")
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.")
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.")
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.")
+        break;
+      default:
+        break;
+    }
+  }
+
+  const addMarker = (coords) => {
+    if (coords !== currentLocation) {
+      setLabel("End")
+    }
+    setId((id)=>id+1);
+    setMarkers((markers) => markers.concat([{ coords, id }]))
+  }
+
+  return isLoaded && center &&(
+    <div>
+      <LoadScript
+      googleMapsApiKey={apiKey}
+      >
+        <GoogleMap
+          mapContainerStyle={{
+            height: "50vh",
+            width: "50vw",
+            margin: "20px",
+          }}
+          zoom={12}
+          center={center}
+          onLoad={map => {
+            addMarker(currentLocation)
+            setMyMap(map)
+            }
+          }
+          onClick={(e)=> createMark && markers.length < 2 ? addMarker(e.latLng.toJSON()) : null}
+    >{markers ? (
+      markers.map((marker) => {
+        return (
+          <Marker
+            key={marker.id}
+            draggable={createMark}
+            position={marker.coords}
+            onDragEnd={e => marker.coords = e.latLng.toJSON()}
+            label = {marker.id === 0 ? "start" : "end"}
+          />
+        )
+      })
+    ) : null }
+      
+    </GoogleMap>
+    <button
+      type="button"
+      style={{backgroundColor: createMark ? "green" : null}}
+      onClick={()=>{setcreateMark(()=>!createMark)}}
+    >ADD & DRAG</button>
+    <button
+      type="button"
+      onClick={()=>setMarkers([])}
+        >CLEAR MAP</button>
+    </LoadScript>
+  </div>
+  )
+
+  // return renderMap();
 }
 
 export default UsersList;
