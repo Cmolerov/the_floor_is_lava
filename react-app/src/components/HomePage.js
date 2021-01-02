872a1e8091092a7a1ba9565b7c072fc294bc8a56
carlos molero <cmolero130@gmail.com>
2021-01-02 13:02:48 -0500

    card homepage

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -1,48 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import './HomePage.css';
-import * as routesAction from '../store/routes'
+import { useSelector, useDispatch } from "react-redux";
+import "./HomePage.css";
+import * as routesAction from "../store/routes";
 
 export default function HomePage(props) {
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+    const routes = useSelector((state) => state.routes.routes);
     let id;
-    if (props.authenticated) {
-        id = props.user.id
-  }
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const routes = useSelector(state => state.routes.routes);
-    
-    useEffect(() => {
-        dispatch(routesAction.routesSearch(id))
-            .then(() => setIsLoaded(true))
-      }, [dispatch])
 
-    return isLoaded &&(
-        <div className="homePage_container">
-            <div className="homePage_container_container">
-                <div className="homePage_container-left">
-                    <div className="homePage_container-left_top">
-                        <h1>Profile</h1>
+    useEffect(() => {
+        dispatch(routesAction.routesSearch(id)).then(() => setIsLoaded(true));
+    }, [dispatch]);
+
+    if (props.authenticated) {
+        id = props.user.id;
+    }
+    return (
+        isLoaded && (
+            <div className="homePage_container">
+                <div className="homePage_container_container">
+                    <div className="homePage_container-left">
+                        <div className="homePage_container-left_top">
+                            <h1>Profile</h1>
+                        </div>
+                        <div className="homePage_container-left_bottom">
+                            <h1>This Week</h1>
+                        </div>
                     </div>
-                    <div className="homePage_container-left_bottom">
-                        <h1>This Week</h1>
+                    <div className="homePage_container-middle">
+                        <div className="homePage_container-middle-container">
+                            <h1>Activity of the Week</h1>
+                            <div className="homePage_routesCard">
+                                <p>Name: {routes[1].name}</p>
+                                <p>Description: {routes[1].description}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="homePage_container-middle">
-                    <div className="homePage_container-middle-container">
-                        <h1>Activity of the Week</h1>
-                        <p>{routes[1].name}</p>
-                    </div>
-                </div>
-                <div className="homePage_container-right">
-                    <div className="homePage_container-left_top">
-                        <h1>Explore New Routes</h1>
-                    </div>
-                    <div className="homePage_container-left_bottom">
-                        <h1>Route of the Day</h1>
+                    <div className="homePage_container-right">
+                        <div className="homePage_container-left_top">
+                            <h1>Explore New Routes</h1>
+                        </div>
+                        <div className="homePage_container-left_bottom">
+                            <h1>Route of the Day</h1>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        )
     );
 }
