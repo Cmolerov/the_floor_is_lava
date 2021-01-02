cde889dd844bd2459cae4f16b3c1066a344065f3
dale.sakamoto <daletsakamoto@gmail.com>
2021-01-01 19:10:07 -0800

    single-route addition

--- react-app/src/components/SingleRoute.js
+++ react-app/src/components/SingleRoute.js
@@ -9,6 +9,7 @@ import * as routesAction from '../store/routes';
 function SingleRoute() {
 
   const [currentLocation, setCurrentLocation] = useState(null)
+  const [running, setRunning] = useState(false)
   
   const route = useSelector(state => state.routes.route);
   const origin = useSelector(state => `${state.routes.route.startLat},${state.routes.route.startLong}`);
@@ -79,6 +80,9 @@ function SingleRoute() {
     }
   }
 
+  const runRoute = () => {
+    setRunning(true)
+  };
   
   useEffect(() => {
     dispatch(routesAction.routeSearch(routeId))
@@ -86,66 +90,74 @@ function SingleRoute() {
       .then(() => setIsLoaded2(true))
   }, [])
 
-    return isLoaded2 && currentLocation &&(
-      <div className="main__div__map-container">
-        <p className="route__p__name">{ route.name }</p>
-        <LoadScript
-          googleMapsApiKey={apiKey}
-        >
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
+  return isLoaded2 && currentLocation && (
+      <div className='single__route__container'>
+        <h1 className="route__p__name">{ route.name }</h1>
+        <div className="main__div__map-container">
+          <LoadScript
+            googleMapsApiKey={apiKey}
           >
-            {
-              (
-                destination !== '' &&
-                origin !== ''
-              ) && (
-                <DirectionsService
-                  // required
-                  options={{ 
-                    destination: destination,
-                    origin: origin,
-                    travelMode: travelMode
-                  }}
-                  // required
-                  callback={directionsCallback}
-                  // optional
-                  onLoad={directionsService => {
-                    console.log('DirectionsService onLoad directionsService: ', directionsService)
-                  }}
-                />
-              )
-            }
-  
-            {
-              response !== null && (
-                <DirectionsRenderer
-                  // required
-                  options={{
-                    directions: response,
-                    suppressMarkers: true
-                  }}
-                  // optional
-                  onLoad={directionsRenderer => {
-                    console.log('DirectionsRenderer onLoad directionsRenderer: ', directionsRenderer)
-                  }}
-                />
-              )
-            }
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+            >
+              {
+                (
+                  destination !== '' &&
+                  origin !== ''
+                ) && (
+                  <DirectionsService
+                    // required
+                    options={{ 
+                      destination: destination,
+                      origin: origin,
+                      travelMode: travelMode
+                    }}
+                    // required
+                    callback={directionsCallback}
+                    // optional
+                    onLoad={directionsService => {
+                      console.log('DirectionsService onLoad directionsService: ', directionsService)
+                    }}
+                  />
+                )
+              }
+    
+              {
+                response !== null && (
+                  <DirectionsRenderer
+                    // required
+                    options={{
+                      directions: response,
+                      suppressMarkers: running ? true : false
+                    }}
+                    // optional
+                    onLoad={directionsRenderer => {
+                      console.log('DirectionsRenderer onLoad directionsRenderer: ', directionsRenderer)
+                    }}
+                  />
+                )
+              }
+              {running ?
+                <>
+                <Marker
+                key={1}
+                position={start}
+                icon={'https://img.icons8.com/office/30/000000/running.png'}
+              />
               <Marker
-              key={1}
-              position={start}
-              icon={'https://img.icons8.com/office/30/000000/running.png'}
-            />
-            <Marker
-              key={2}
-              position={end}
-              icon={'https://img.icons8.com/fluent-systems-filled/24/000000/finish-flag.png'}
-            />
-          </GoogleMap>
-        </LoadScript>
+                key={2}
+                position={end}
+                icon={'https://img.icons8.com/fluent-systems-filled/24/000000/finish-flag.png'}
+                  />
+                  </>
+                :
+                null}
+            </GoogleMap>
+          </LoadScript>
+        </div>
+        <button onClick={runRoute} className="single__route__run__button">Run Route</button>
       </div>
     )
 }
