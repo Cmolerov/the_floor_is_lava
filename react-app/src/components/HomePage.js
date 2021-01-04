c7a97b28a6fb808c1a83e766993e2871bd4decd0
carlos molero <cmolero130@gmail.com>
2021-01-03 19:51:26 -0500

    addd nav link to routes

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
 // import "./HomePage.css";
 import * as routesAction from "../store/routes";
 
@@ -34,70 +35,120 @@ export default function HomePage(props) {
                             </div>
                         </div>
                         <div className="homePage_container-left_bottom">
-                            <h2>This Week</h2>
+                            <div className="homePage_profile">
+                                <h2>This Week</h2>
+                            </div>
                         </div>
                     </div>
                     <div className="homePage_container-middle">
                         <div className="homePage_container-middle-container">
                             <h2>Activity of the Week</h2>
-                            <div className="homePage_routesCard">
-                                <p>
-                                    {routes[1].name} || Distance:{" "}
-                                    {routes[1].distance}
-                                </p>
-                                {/* <p>Description: {routes[1].description}</p> */}
-                                <p>
-                                    <p>Avg. Pace: 17 min</p>
-                                </p>
-                            </div>
-                            <div className="homePage_routesCard">
-                                <p>
-                                    {routes[2].name} || Distance:{" "}
-                                    {routes[2].distance}
-                                </p>
-                                <p>
-                                    <p>Avg. Pace: 21 min</p>
-                                </p>
-                                {/* <p>Description: {routes[2].description}</p> */}
-                            </div>
-                            <div className="homePage_routesCard">
-                                <p>
-                                    {routes[3].name} || Distance:{" "}
-                                    {routes[3].distance}
-                                </p>
-                                <p>
-                                    <p>Avg. Pace: 9 min</p>
-                                </p>
-                                {/* <p>Description: {routes[3].description}</p> */}
-                            </div>
-                            <div className="homePage_routesCard">
-                                <p>
-                                    {routes[4].name} || Distance:{" "}
-                                    {routes[4].distance}
-                                </p>
-                                <p>
-                                    <p>Avg. Pace: 94 min</p>
-                                </p>
-                                {/* <p>Description: {routes[4].description}</p> */}
-                            </div>
+                            <NavLink
+                                style={{
+                                    textDecoration: "none",
+                                    color: "inherit",
+                                }}
+                                to={`/routes/${routes[1].id}`}
+                            >
+                                <div className="homePage_routesCard">
+                                    <p>
+                                        {routes[1].name} || Distance:{" "}
+                                        {routes[1].distance}
+                                    </p>
+                                    {/* <p>Description: {routes[1].description}</p> */}
+                                    <p>
+                                        <p>Avg. Pace: 17 min</p>
+                                    </p>
+                                </div>
+                            </NavLink>
+                            <NavLink
+                                style={{
+                                    textDecoration: "none",
+                                    color: "inherit",
+                                }}
+                                to={`/routes/${routes[2].id}`}
+                            >
+                                <div className="homePage_routesCard">
+                                    <p>
+                                        {routes[2].name} || Distance:{" "}
+                                        {routes[2].distance}
+                                    </p>
+                                    <p>
+                                        <p>Avg. Pace: 21 min</p>
+                                    </p>
+                                    {/* <p>Description: {routes[2].description}</p> */}
+                                </div>
+                            </NavLink>
+                            <NavLink
+                                style={{
+                                    textDecoration: "none",
+                                    color: "inherit",
+                                }}
+                                to={`/routes/${routes[3].id}`}
+                            >
+                                <div className="homePage_routesCard">
+                                    <p>
+                                        {routes[3].name} || Distance:{" "}
+                                        {routes[3].distance}
+                                    </p>
+                                    <p>
+                                        <p>Avg. Pace: 9 min</p>
+                                    </p>
+                                    {/* <p>Description: {routes[3].description}</p> */}
+                                </div>
+                            </NavLink>
+                            <NavLink
+                                style={{
+                                    textDecoration: "none",
+                                    color: "inherit",
+                                }}
+                                to={`/routes/${routes[4].id}`}
+                            >
+                                <div className="homePage_routesCard">
+                                    <p>
+                                        {routes[4].name} || Distance:{" "}
+                                        {routes[4].distance}
+                                    </p>
+                                    <p>
+                                        <p>Avg. Pace: 94 min</p>
+                                    </p>
+                                    {/* <p>Description: {routes[4].description}</p> */}
+                                </div>
+                            </NavLink>
                         </div>
                     </div>
                     <div className="homePage_container-right">
                         <div className="homePage_container-left_top">
-                            <div className="homePage_newRoutes">
-                                <h2>Explore New Routes</h2>
-                                <p>Path: {routes[9].name} </p>
-                                <p> Distance: {routes[9].distance}</p>
-                                <p>Avg. Time: 57 min</p>
-                            </div>
+                            <NavLink
+                                style={{
+                                    textDecoration: "none",
+                                    color: "inherit",
+                                }}
+                                to={`/routes/${routes[9].id}`}
+                            >
+                                <div className="homePage_newRoutes">
+                                    <h2>Explore New Routes</h2>
+                                    <p>Path: {routes[9].name} </p>
+                                    <p> Distance: {routes[9].distance}</p>
+                                    <p>Avg. Time: 57 min</p>
+                                </div>
+                            </NavLink>
                         </div>
-                        <div className="homePage_container-left_bottom">
-                            <div className="homePage_routeOfDay">
-                                <h2>Route of the Day</h2>
-                                <p>{routes[6].name}</p>
-                                <p>Distance: {routes[6].distance}</p>
+                        <NavLink
+                            style={{
+                                textDecoration: "none",
+                                color: "inherit",
+                            }}
+                            to={`/routes/${routes[6].id}`}
+                        >
+                            <div className="homePage_container-left_bottom">
+                                <div className="homePage_routeOfDay">
+                                    <h2>Route of the Day</h2>
+                                    <p>{routes[6].name}</p>
+                                    <p>Distance: {routes[6].distance}</p>
+                                </div>
                             </div>
-                        </div>
+                        </NavLink>
                     </div>
                 </div>
             </div>
